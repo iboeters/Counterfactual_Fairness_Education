23d05e48801353e4105946b69c82b1ca09b87f95
Iris <iboeters@LAPTOP-MGJ0DHHN.localdomain>
2021-10-18 09:39:03 +0200

    CEVAE work

--- CEVAE/main.py
+++ CEVAE/main.py
@@ -5,8 +5,9 @@ import sys
 import torch
 from sklearn.model_selection import train_test_split
 from collections import defaultdict
-from CEVAE import CEVAE_model
 from argparse import ArgumentParser
+from CEVAE import CEVAE_model
+from fairness_unaware import fairness_unaware
 
 cwd = os.getcwd()
 n_con = 2
@@ -50,62 +51,69 @@ print(df.columns.get_loc("ADV_TEST_11.0"))
 
 #split train test set
 df = pd.DataFrame.to_numpy(df)
+#a = sensitive attributes, y=level after 3 yrs, s_con=SES continuous vars, s_bin=SES binary variables, l=language level, t=teachers advice, e=EPST test advice, f=final advice
 #a: 0-7
 #y: 7
-#x_SES_con: 8-10
-#x_SES_bin: 10-48
-#x_lang: 48-52
-#x_teach: 52-62
-#x_test: 62-70
-#x_fin: 70-80
+#s_con: 8-10
+#s_bin: 10-48
+#l: 48-52
+#t: 52-62
+#e: 62-70
+#f: 70-80
 
-a, y, x_SES_con, x_SES_bin, x_lang, x_teach, x_test, x_fin = df[:,0:7], df[:,7][:, np.newaxis], df[:,8:10], df[:,10:48], df[:,48:52], df[:,52:62], df[:,62:70], df[:,70:80]
+a, y, s_con, s_bin, l, t, e, f = df[:,0:7], df[:,7][:, np.newaxis], df[:,8:10], df[:,10:48], df[:,48:52], df[:,52:62], df[:,62:70], df[:,70:80]
 
 a = a.astype(int)
 y = y.astype(int)
-x_SES_con = x_SES_con.astype(float)
-x_SES_bin = x_SES_bin.astype(int)
-x_lang = x_lang.astype(int)
-x_teach = x_teach.astype(int)
-x_test = x_test.astype(int)
-x_fin = x_fin.astype(int)
+s_con = s_con.astype(float)
+s_bin = s_bin.astype(int)
+l = l.astype(int)
+t = t.astype(int)
+e = e.astype(int)
+f = f.astype(int)
 
 print("Dimensions of our variables:")
 print(a.shape)
 print(y.shape)
-print(x_SES_con.shape)
-print(x_SES_bin.shape)
-print(x_lang.shape)
-print(x_teach.shape)
-print(x_test.shape)
-print(x_fin.shape)
+print(s_con.shape)
+print(s_bin.shape)
+print(l.shape)
+print(t.shape)
+print(e.shape)
+print(f.shape)
 n_obs = a.shape[0]
 
-# data = [a, y, x_SES_con, x_SES_bin, x_lang, x_teach, x_test, x_fin]
+# data = [a, y, s_con, s_bin, l, t, e, f]
 
 # make an array from 0 to n observations
 arr_i = np.arange(n_obs)
 # use split on array as indexes for our variables
-i_tr, i_te = train_test_split(arr_i, test_size= 0.1, random_state= 42)
+i_train, i_test = train_test_split(arr_i, test_size= 0.1, random_state= 42)
 
-train_data = [a[i_tr], y[i_tr], x_SES_con[i_tr], x_SES_bin[i_tr], x_lang[i_tr], x_teach[i_tr], x_test[i_tr], x_fin[i_tr]]
-test_data = [a[i_te], y[i_te], x_SES_con[i_te], x_SES_bin[i_te], x_lang[i_te], x_teach[i_te], x_test[i_te], x_fin[i_te]]
+train_data = [a[i_train], y[i_train], s_con[i_train], s_bin[i_train], l[i_train], t[i_train], e[i_train], f[i_train]]
+test_data = [a[i_test], y[i_test], s_con[i_test], s_bin[i_test], l[i_test], t[i_test], e[i_test], f[i_test]]
 
-print(i_tr)
-print(i_te)
+print(i_train)
+print(i_test)
 print(train_data[0].shape)
 print(test_data[0].shape)
 print(train_data[0].shape[0]/n_obs)
 print(test_data[0].shape[0]/n_obs)
 
-a_tr, y_tr, x_SES_con_tr, x_SES_bin_tr, x_lang_tr, x_teach_tr, x_test_tr, x_fin_tr = train_data
-x_SES_tr = np.hstack((x_SES_con_tr, x_SES_bin_tr))
+a_train, y_train, s_con_train, s_bin_train, l_train, t_train, e_train, f_train = train_data
+a_test, y_test, s_con_test, s_bin_test, l_test, t_test, e_test, f_test = test_data
+x_SES_train = np.hstack((s_con_train, s_bin_train))
 
 # check if cuda is available -> false
 print(torch.cuda.is_available())
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # if cuda => run on gpu
 
-CEVAE = CEVAE_model(args=args, dim_a=a_tr.shape[1], dim_y=y_tr.shape[1], dim_s_con=x_SES_con_tr.shape[1], dim_s_bin=x_SES_bin_tr.shape[1], dim_l=x_lang_tr.shape[1], dim_t=x_teach_tr.shape[1], dim_e=x_test_tr.shape[1], dim_f=x_fin_tr.shape[1], dim_z=args.zDim, dim_q_h=args.hDim, dim_p_h=args.hDim).to(args.device)
+x_train = s_con_train, s_bin_train, l_train, t_train, e_train, f_train
+x_test = s_con_test, s_bin_test, l_test, t_test, e_test, f_test
+# fairness through unawareness
+fairness_unaware(args, x_train, x_test, y_train, y_test)
+
+CEVAE = CEVAE_model(args=args, dim_a=a_train.shape[1], dim_y=y_train.shape[1], dim_s_con=s_con_train.shape[1], dim_s_bin=s_bin_train.shape[1], dim_l=l_train.shape[1], dim_t=t_train.shape[1], dim_e=e_train.shape[1], dim_f=f_train.shape[1], dim_z=args.zDim, dim_q_h=args.hDim, dim_p_h=args.hDim).to(args.device)
 
 print(args.hDim)
 
@@ -120,3 +128,5 @@ print(args.hDim)
 
 #save models
 #plot loss
+
+
